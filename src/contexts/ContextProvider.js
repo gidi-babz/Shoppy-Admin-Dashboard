e226b9ccd396aed22bc6051a3f3cb8c197a7c8ad
Gidi Babz <tunex4ind@gmail.com>
2022-08-20 14:56:47 +0100

    more commits

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,3 +1,4 @@
+import { HandWrittenSignatureSettings } from '@syncfusion/ej2/pdfviewer';
 import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
@@ -11,11 +12,22 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
 	const [activeMenu, setActiveMenu] = useState(true);
+	const [isClicked, setIsClicked] = useState(initialState);
+	const [screenSize, setScreenSize] = useState(undefined);
+
+	const handleClick = (clicked) =>
+		setIsClicked({ ...initialState, [clicked]: true });
+
 	return (
 		<StateContext.Provider
 			value={{
 				activeMenu,
 				setActiveMenu,
+				isClicked,
+				setIsClicked,
+				handleClick,
+				screenSize,
+				setScreenSize,
 			}}
 		>
 			{children}
