03587170815dd84a8c1ab350679f9f8db21c34c3
Gidi Babz <tunex4ind@gmail.com>
2022-08-23 00:11:01 +0100

    editor app

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { Header } from '../components';
 const ColorPicker = () => {
-	return <div>ColorPicker</div>;
+	return (
+		<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+			<Header category="App" title="Color Picker" />
+		</div>
+	);
 };
 
 export default ColorPicker;
